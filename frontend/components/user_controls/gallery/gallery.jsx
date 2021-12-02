474432afa3b112318b599cd4956a3c7bd32b6d18
mjlomeli <mjlomeli@uci.edu>
2021-12-02 07:42:47 -0800

    nothing really changed

--- frontend/components/user_controls/gallery/gallery.jsx
+++ frontend/components/user_controls/gallery/gallery.jsx
@@ -3,36 +3,6 @@ import {v4 as uuidv4} from "uuid";
 import React from 'react';
 import './gallery.css'
 import GridLayout from "../grid_layout/grid_layout";
-import {fetchUserControl, updateUserControl} from "../../../actions/ui_user_control_actions";
-
-
-const mapStateToProps = (state, ownProps) => {
-    let userControl = state.ui.userControl.gallery;
-    let galleryId = (ownProps.match && ownProps.match.params.id) || ownProps.galleryId;
-    let gallery = (galleryId && userControl[galleryId]) || null;
-    let component = gallery && gallery.component || null;
-    let images = gallery && gallery.images || null;
-    let carousel = gallery && gallery.carousel || null;
-
-
-    return {
-        //errors: errors.session, // need to add a ui or user_control errors
-        galleryId: galleryId,
-        gallery: gallery,
-        component: component,
-        reducer: "gallery",
-        images: images,
-        carousel: carousel
-    }
-};
-
-const mapDispatchToProps = (dispatch, ownProp) => {
-    return {
-        fetchUserControl: (reducer, id) => dispatch(fetchUserControl(reducer, id)),
-        updateUserControl: (reducer, id, ui) => dispatch(updateUserControl(reducer, id, ui))
-    }
-};
-
 
 let defaultGallery = {
     images: [
@@ -44,77 +14,47 @@ let defaultGallery = {
     galleryIndex: 0
 }
 
-
 class Gallery extends React.Component {
-    static REDUCER = "gallery";
     constructor(props) {
         super(props);
-        this.state = {
-            carousel: props.carousel || defaultGallery.galleryImage,
-            images: props.images || defaultGallery.images,
-            lowerImage: defaultGallery.galleryImage,
-            index: 0,
-            components: {
-                'carousel':<img className="gallery-carousel" src={defaultGallery.galleryImage} alt="carousel" />,
-                '0': <img onClick={() => this.setImage(0)} src={defaultGallery.images[0]} alt="img"/>,
-                '1': <img onClick={() => this.setImage(1)} src={defaultGallery.images[1]} alt="img"/>,
-                '2': <img onClick={() => this.setImage(2)} src={defaultGallery.images[2]} alt="img"/>,
-                '3': <img onClick={() => this.setImage(3)} src={defaultGallery.images[3]} alt="img"/>
-            },
-            component: props.component
-        }
-        this.id = this.props.galleryId || this.props.match && this.props.match.params.id;
+        this.state = {images: [], galleryImage: null, galleryIndex: 0}
     }
 
-    isValid(){
-        return this.props.component ||
-            this.id && this.props.fetchUserControl(Gallery.REDUCER, this.id) ||
-            this.state.ui || false;
+    componentDidMount() {
+        if (this.props.images)
+            this.setState({images: [...this.props.images], galleryImage: this.props.images[0], galleryIndex: 0, img: <img id="gallery-image" className="gallery-carousel" src="https://i.etsystatic.com/21035245/r/il/39a4e2/2038971261/il_794xN.2038971261_9zrx.jpg" alt="img"/>})
+        else
+            this.setState({...defaultGallery});
     }
 
-    resolve() {
+    setImage(index) {
+        this.setState({galleryImage: this.state.images[index], galleryIndex: index});
+    }
+
+    render() {
+        if (!this.state.images || !this.state.images.length)
+            return null;
+
         let areas = [
             "image0 carousel carousel carousel",
             "image1 carousel carousel carousel",
             "image2 carousel carousel carousel",
             "image3 carousel carousel carousel",
         ];
-
         let components = {
-            'carousel':<img className="gallery-carousel" src={this.state.carousel} alt="carousel" />,
-            'image0': <img onClick={() => this.setImage(0)} src={defaultGallery.images[0]} alt="img"/>,
-            'image1': <img onClick={() => this.setImage(1)} src={defaultGallery.images[1]} alt="img"/>,
-            'image2': <img onClick={() => this.setImage(2)} src={defaultGallery.images[2]} alt="img"/>,
-            'image3': <img onClick={() => this.setImage(3)} src={defaultGallery.images[3]} alt="img"/>
+            'carousel': <img className="gallery-carousel" src={this.state.galleryImage} alt="image"/>,
+            'image0': <img onClick={() => this.setImage(0)} src={this.state.images[0]} alt="img"/>,
+            'image1': <img onClick={() => this.setImage(1)} src={this.state.images[1]} alt="img"/>,
+            'image2': <img onClick={() => this.setImage(2)} src={this.state.images[2]} alt="img"/>,
+            'image3': <img onClick={() => this.setImage(3)} src={this.state.images[3]} alt="img"/>
         }
 
-        this.props.updateUserControl(GridLayout.REDUCER, this.id, {areas: areas, components: components});
-
-        let gallery = <div>
-            <GridLayout gridLayoutId={this.props.galleryId}
-                        classGrid="gallery-grid"
-                        classItems="gallery-items"/>
-            {this.state.components['carousel']}
+        return <div>
+            <GridLayout areas={areas} components={components} classGrid="gallery-grid" classItems="gallery-items"/>
+            <br />
+            <img className="gallery-carousel" src={this.state.galleryImage} alt="image"/>
         </div>
-
-        this.props.updateUserControl(Gallery.REDUCER, this.id,
-            {component: gallery, carousel: this.state.carousel, images: this.state.images})
-        return null;
-    }
-
-    setImage(index, e) {
-        let components = this.state.components;
-        components['carousel'] = this.state.components[index]
-        this.setState({carousel: this.state.images[index], components, lowerImage: this.state.images[index]})
-    }
-
-    render() {
-        if (!this.isValid())
-            return this.resolve();
-
-        return this.props.fetchUserControl(Gallery.REDUCER, this.id).component ||
-            this.props.component || null
     }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Gallery)
+export default Gallery
