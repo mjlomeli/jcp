75929765f63acbd73a6f2a0f8cea475fcd4694a0
mjlomeli <mjlomeli@uci.edu>
2021-12-23 12:43:51 -0800

    added favorites to card list

--- frontend/components/user_controls/card_listing/card_listing.jsx
+++ frontend/components/user_controls/card_listing/card_listing.jsx
@@ -96,7 +96,7 @@ class CardListing extends React.Component {
         return title;
     }
 
-    favoriteComponent(itemId){
+    favoriteComponent(productId){
         return <a href="#" className="card-listing-favorite">
             <svg className="card-listing-favorite-image" version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="510px" height="459px" viewBox="51 166.5 510 459" enableBackground="new 51 166.5 510 459">
                 <path d="M306,625.5c-42.101,0-255-160.956-255-306c0-87.235,60.282-153,140.25-153c43.391,2.685,84.259,21.312,114.75,52.301 c30.548-30.907,71.383-49.519,114.75-52.301c79.968,0,140.25,65.765,140.25,153C561,464.544,348.101,625.5,306,625.5z M191.25,217.5 c-51.714,0-89.25,42.917-89.25,102c0,104.754,164.016,237.787,204,255c39.882-16.754,204-148.716,204-255 c0-59.083-37.536-102-89.25-102c-50.465,0-94.35,53.678-94.886,54.238l-20.094,24.812l-19.763-24.99 C285.243,270.617,242.25,217.5,191.25,217.5z"/>
@@ -107,7 +107,7 @@ class CardListing extends React.Component {
     imageComponent(productId, imageUrl){
         return <div>
             <div className="card-listing-image-div">
-                {this.favoriteComponent()}
+                {this.favoriteComponent(productId)}
                 <Link to={`/product/${productId}`} style={{textDecoration: 'none'}}>
                     <img className="card-listing-image" alt="img" aria-hidden="true" src={imageUrl}/>
                 </Link>
@@ -134,6 +134,8 @@ class CardListing extends React.Component {
     }
 
     tagComponent(productId, tag, tags){
+        if (!tag || !tags)
+            return null;
         return <Link to={`/product/${productId}`} style={{textDecoration: 'none'}}>
             <Tags tag={tag} tags={tags} />
         </Link>
