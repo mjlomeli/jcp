666c3dd31ab2313eb4a17c9567d7749aabf65f23
mjlomeli <mjlomeli@uci.edu>
2021-12-23 09:57:27 -0800

    IMPORTANT: this is likely a safe setup before changes were made. This node might have the speed enhancement. Started products template.

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -8,32 +8,29 @@ import {
     HashRouter
 } from 'react-router-dom';
 
-import * as r from "./../utils/review_util"
-import * as c from "./../utils/cart_item_util"
-
 import GreetingContainer from './greeting/greeting_container';
 import SignUpFormContainer from './session_form/signup_form_container';
 import LogInFormContainer from './session_form/login_form_container';
 import DemoLogInFormContainer from './session_form/demo_form_container';
 import { AuthRoute, ProtectedRoute } from '../utils/route_util';
- import Gallery from "./user_controls/gallery/gallery";
-// import NavbarLayout from "./user_controls/navbar/navbar";
-// import Home from "./pages/home";
-// import Products from "./pages/products";
-// import Cart from "./pages/cart";
-// import CartItem from "./user_controls/cart_item/cart_item";
-// import FooterTemplate from "./themes/footer/footer_template";
-// import CardFeatured from "./user_controls/card_featured/card_featured";
-// import CardListing from "./user_controls/card_listing/card_listing";
-// import CardThumbnail from "./user_controls/card_thumbnail/card_thumbnail";
-// import CircularThumbnail from "./user_controls/circular_thumbnail/circular_thumbnail";
-// import Rating from "./user_controls/rating/rating";
-// import SearchBar from "./user_controls/searchbar/searchbar";
-// import ProductsTemplate from "./themes/products_template/products_template";
-// import ProductsList from "./user_controls/product_list/products_list";
-// import FlowLayout from "./user_controls/flow_layout/flow_layout";
-// import CartTemplate from "./themes/cart_template/cart_template";
-// import ProductTemplate from "./themes/product_template/product_template";
+import Gallery from "./user_controls/gallery/gallery";
+import NavbarLayout from "./user_controls/navbar/navbar";
+import Home from "./pages/home";
+import Products from "./pages/products";
+import Cart from "./pages/cart";
+import CartItem from "./user_controls/cart_item/cart_item";
+import FooterTemplate from "./themes/footer/footer_template";
+import CardFeatured from "./user_controls/card_featured/card_featured";
+import CardListing from "./user_controls/card_listing/card_listing";
+import CardThumbnail from "./user_controls/card_thumbnail/card_thumbnail";
+import CircularThumbnail from "./user_controls/circular_thumbnail/circular_thumbnail";
+import Rating from "./user_controls/rating/rating";
+import SearchBar from "./user_controls/searchbar/searchbar";
+import ProductsTemplate from "./themes/products_template/products_template";
+import ProductsList from "./user_controls/product_list/products_list";
+import FlowLayout from "./user_controls/flow_layout/flow_layout";
+import CartTemplate from "./themes/cart_template/cart_template";
+import ProductTemplate from "./themes/product_template/product_template";
 import GridLayout from "./user_controls/grid_layout/grid_layout";
 
 /* learn more about grid-auto-flow */
@@ -59,22 +56,22 @@ const App = () => (
             {/*<Route exact path="/gallery" component={Gallery} />*/}
             {/*<ProtectedRoute exact path="/cart_template" component={CartTemplate} />*/}
             {/*<Route exact path="/card_featured" component={CardFeatured} />*/}
-            {/*<Route exact path="/card_listing" component={CardListing} />*/}
+            <Route exact path="/card_listing" component={CardListing} />
             {/*<Route exact path="/card_thumbnail" component={CardThumbnail} />*/}
             {/*<Route exact path="/cart_item" component={CartItem} />*/}
             {/*<Route exact path="/circular_thumbnail" component={CircularThumbnail} />*/}
-            <Route exact path="/gallery/:id" component={Gallery} />
+            {/*<Route exact path="/gallery/:id" component={Gallery} />*/}
             {/*<Route exact path="/rating" component={Rating} />*/}
             {/*<Route exact path="/searchbar" component={SearchBar} />*/}
             {/*<Route exact path="/footer" component={FooterTemplate} />*/}
             {/*<Route exact path="/products_list" component={ProductsList} />*/}
-            {/*<Route exact path="/products_template" component={ProductsTemplate} />*/}
+            <Route exact path="/products_template" component={ProductsTemplate} />
             {/*<Route exact path="/flow_layout" component={FlowLayout} />*/}
             {/*<Route exact path="/rating" component={Rating} />*/}
             {/*<Route exact path="/card_listing/:id" component={CardListing} />*/}
             {/*<Route exact path="/product/:id" component={ProductTemplate} />*/}
             {/*<Route exact path="/grid_layout/:id" component={GridLayout} />*/}
-            <Route exact path="/grid_layout" component={GridLayout} />
+            {/*<Route exact path="/grid_layout" component={GridLayout} />*/}
         </Switch>
         {/*END OF DON'T REMOVE*/}
 
--- frontend/components/user_controls/card_listing/card_listing.jsx
+++ frontend/components/user_controls/card_listing/card_listing.jsx
@@ -13,63 +13,61 @@ import Rating from "../rating/rating";
 class Price extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {}
     }
 
-    calculatedPrice(){
-        if (this.props.discount){
-            let price = this.props.price || 0;
-            let discount = this.props.discount || 0;
-            let total = price - (price * discount);
-            return <><span className="global-card-listing-calculated-price">${total.toFixed(2)}</span></>;
-        }
+    priceComponent() {
+        let price = this.props.price || 0;
+        let discount = this.props.discount || 0;
+        price = price - (price * discount);
+        return <><span className="global-card-listing-calculated-price">${price.toFixed(2)}</span></>;
     }
 
-    discounted(){
-        let percentage = (this.props.discount) ? this.props.discount * 100 >> 0 : 0
-        if (percentage)
-            return <><span className="global-card-listing-discount">({percentage}% off)</span></>
-        return <></>
+    discountedComponent() {
+        if (!this.props.discount)
+            return null;
+        return <span className="global-card-listing-original-price">
+            ${this.props.price}
+        </span>
+    }
+
+    percentOffComponent() {
+        if (!this.props.discount)
+            return null;
+        let percentage = this.props.discount * 100 >> 0;
+        return <><span className="global-card-listing-discount">({percentage}% off)</span></>
     }
 
     render() {
-        return <>
-            <Route className="global-card-listing-price-container">
-                {this.calculatedPrice()}
-                <span className="global-card-listing-original-price">
-                    ${this.props.price}
-                </span>
-                {this.discounted()}
-            </Route>
-        </>
+        return <div className="global-card-listing-price-container">
+            {this.priceComponent()}
+            {this.discountedComponent()}
+            {this.percentOffComponent()}
+        </div>
     }
 }
 
-class Additional extends React.Component {
+class Tags extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {}
     }
 
-    shipping(){
-        return <>
-            <div className="global-card-listing-additional">
-                <label className="global-card-listing-additional-label">FREE shipping</label>
-            </div>
-        </>
-    }
-
-    recommendation(){
-        return <>
-            <div className="global-card-listing-additional">
-
-            </div>
-        </>
+    tagComponent(label, color, key){
+        return <div className="global-card-listing-additional" key={key}>
+            <label className="global-card-listing-additional-label" style={{backgroundColor: color}}>{label}</label>
+        </div>
     }
 
     render() {
+        let labels = [this.props.tag] || this.props.tags || [];
         return <>
-            {this.shipping()}
+            {labels.map((label, index) => {
+                switch (label.toLowerCase()) {
+                    case "free shipping":
+                        return this.tagComponent("FREE shipping", '#D4E9D7', index);
+                    default:
+                        return this.tagComponent("No Label", '#000222', index);
+                }
+            })}
         </>
     }
 }
@@ -82,7 +80,7 @@ let defaultListing = {
     store: "Plexida",
     price: 25.99,
     discount: 0.2,
-    freeShipping: true,
+    label: ['free shipping'],
     link: "/product_template"
 }
 
@@ -90,7 +88,6 @@ class CardListing extends React.Component {
     constructor(props) {
         // fetchProduct(productId)
         super(props);
-
         this.state = {}
     }
 
@@ -103,7 +100,7 @@ class CardListing extends React.Component {
         }
     }
 
-    resize(title){
+    resize(title) {
         if (title.length > 70) {
             return `${title.slice(0, 65)}...`
         }
@@ -113,26 +110,28 @@ class CardListing extends React.Component {
     render() {
         if (!this.state.listing)
             return null;
-        let {id, link, image_urls, imageUrl, price, rating, ratingCount, discount, store, quantity,
-            store_id, title, user_id, views} = this.state.listing;
+        let {
+            id, link, image_urls, imageUrl, price, rating, ratingCount, discount, store, quantity,
+            store_id, title, user_id, views
+        } = this.state.listing;
         imageUrl = (image_urls && image_urls[0]) || imageUrl
         let areas = ['image', 'title', 'rating', 'price', 'additional']
         let components = {
             'image': <div className="global-card-listing-image-div">
                 <img className="global-card-listing-image" alt="img"
-                     aria-hidden="true" src={imageUrl} />
+                     aria-hidden="true" src={imageUrl}/>
             </div>,
             'title': <label className="global-card-listing-title">{this.resize(title)}</label>,
             'rating': <Rating rating={rating} count={ratingCount} disabled={true}/>,
             'price': <Price price={price} discount={discount}/>,
             'additional': <Additional freeShipping={true}/>
         }
-        return <Link to={`/product/${id}`}>
-        <GridLayout areas={areas}
-                           components={components}
-                           classGrid="global-card-listing-grid"
-                           classElements="global-card-listing-items" />
-            </Link>
+        return <Link to={`/product/${id}`} style={{textDecoration: 'none'}}>
+            <GridLayout areas={areas}
+                        components={components}
+                        className="global-card-listing-grid"
+                        classElements="global-card-listing-items"/>
+        </Link>
     }
 }
 
